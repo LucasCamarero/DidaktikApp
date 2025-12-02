6ec2f8e398678a97572476ca2d35a367375b4a04
LucasCamarero <l.camareroperez@ikasle.eus>
2025-12-02 12:56:18 +0100

    fix / Estructura general inicial del proyecto (añadido MapScreen.kt)

--- app/src/main/java/com/lucascamarero/didaktikapp/ScreenManager.kt
+++ app/src/main/java/com/lucascamarero/didaktikapp/ScreenManager.kt
@@ -1,4 +1,4 @@
-package com.lucascamarero.didaktikapp.components
+package com.lucascamarero.didaktikapp
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
