a6c9293793688a47915547319a1e8a7864728304
LucasCamarero <l.camareroperez@ikasle.eus>
2025-12-09 21:19:50 +0100

    feat / Seleccionar idioma

--- app/src/main/java/com/lucascamarero/didaktikapp/MainActivity.kt
+++ app/src/main/java/com/lucascamarero/didaktikapp/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.lucascamarero.didaktikapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.lucascamarero.didaktikapp.ui.theme.DidaktikAppTheme
 import com.lucascamarero.didaktikapp.screens.SplashScreen
 import com.lucascamarero.didaktikapp.screens.IntroScreen
+import com.lucascamarero.didaktikapp.viewmodels.LanguageViewModel
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,15 +25,30 @@ class MainActivity : ComponentActivity() {
                 Surface {
                     var showSplash by rememberSaveable { mutableStateOf(true) }
                     var showApp by rememberSaveable { mutableStateOf(false) }
+                    val languageViewModel: LanguageViewModel = viewModel()
 
                     when {
-                        showSplash -> { SplashScreen(onTimeout = { showSplash = false }) }
-                        !showApp -> { IntroScreen(onStartClick = { showApp = true }) }
-                        else -> { ScreenManager() }
+                        showSplash -> {
+                            SplashScreen(
+                                onTimeout = { showSplash = false }
+                            )
+                        }
+
+                        !showApp -> {
+                            IntroScreen(
+                                languageViewModel = languageViewModel,
+                                onStartClick = { showApp = true }
+                            )
+                        }
+
+                        else -> {
+                            ScreenManager(
+                                languageViewModel = languageViewModel
+                            )
+                        }
                     }
                 }
             }
         }
     }
 }
-
--- app/src/main/java/com/lucascamarero/didaktikapp/screens/IntroScreen.kt
+++ app/src/main/java/com/lucascamarero/didaktikapp/screens/IntroScreen.kt
@@ -11,12 +11,24 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.lucascamarero.didaktikapp.components.LanguageCard
+import com.lucascamarero.didaktikapp.models.AppLanguage
+import com.lucascamarero.didaktikapp.viewmodels.LanguageViewModel
 
 @Composable
-fun IntroScreen(onStartClick: () -> Unit) {
+fun IntroScreen(
+    languageViewModel: LanguageViewModel,
+    onStartClick: () -> Unit
+) {
+    var languageIsSelected by rememberSaveable { mutableStateOf(false) }
+
     Column(
         Modifier
             .fillMaxSize()
@@ -24,15 +36,25 @@ fun IntroScreen(onStartClick: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-
-        // se abre screen manager al hacer on click
-        IconButton(onClick = onStartClick) {
-            Icon(
-                imageVector = Icons.Filled.PlayArrow,
-                contentDescription = "Abrir menú",
-                tint = MaterialTheme.colorScheme.secondaryContainer,
-                modifier = Modifier.size(680.dp)
-            )
+        if(languageIsSelected == false) {
+            LanguageCard { lang ->
+                when (lang) {
+                    "eu" -> languageViewModel.changeLanguage(AppLanguage.EUSKERA)
+                    "es" -> languageViewModel.changeLanguage(AppLanguage.CASTELLANO)
+                    "en" -> languageViewModel.changeLanguage(AppLanguage.INGLES)
+                }
+                languageIsSelected = true
+            }
+        } else {
+            // se abre screen manager al hacer on click
+            IconButton(onClick = onStartClick) {
+                Icon(
+                    imageVector = Icons.Filled.PlayArrow,
+                    contentDescription = "Abrir menú",
+                    tint = MaterialTheme.colorScheme.secondaryContainer,
+                    modifier = Modifier.size(680.dp)
+                )
+            }
         }
     }
 }
